014e56336aaaa8b64e59e5a9766de59d64333dc8
SalmanFarsi123 <168709749+SalmanFarsi123@users.noreply.github.com>
2024-12-21 12:55:48 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,17 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.salmanfarsi.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.salmanfarsi.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.salmanfarsi_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.salmanfarsi_subnet[*].id
 }
+
 
